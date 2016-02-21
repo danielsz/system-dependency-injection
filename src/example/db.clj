123766bf5403e3215368af8c93177b0f25e424f6
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2016-02-21 22:41:49 +0200

    Use persistence protocol
    * For maximum elegance

--- src/example/db.clj
+++ src/example/db.clj
@@ -1,6 +1,21 @@
 (ns example.db
-  (:require [yesql.core :refer [defqueries]]))
+  (:refer-clojure :exclude [list])
+  (:require [yesql.core :refer [defqueries]]
+            [system.components.jdbc])
+  (:import [system.components.jdbc JDBCDatabase]))
 
 (defqueries "sql/queries.sql")
 
+(defprotocol DemoPersistence
+  (list [this] "list all users")
+  (save [this name] "save a user")
+  (delete [this name] "get a video by id"))
 
+(extend-type JDBCDatabase
+  DemoPersistence
+  (list [this]
+    (directors {} {:connection (:db-spec this)}))
+  (save [this name]
+    (save-director<! {:name name} {:connection (:db-spec this)}))
+  (delete [this name]
+    (delete-director! {:name name} {:connection (:db-spec this)})))
